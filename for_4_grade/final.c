4e510d778f11c9357e8ce4246ece807c61fac11d
Maxim Perestoronin <116491226+mperestoronin@users.noreply.github.com>
2023-04-23 19:34:51 +0300

    Add files via upload

---
+++ for_4_grade/final.c
@@ -0,0 +1,127 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <semaphore.h>
+
+#define DEFAULT_NUM_THREADS 4
+#define BUFFER_SIZE 256
+
+typedef struct
+{
+    const char *text;
+    char encrypted_text[BUFFER_SIZE];
+    int start_index;
+    int end_index;
+    pthread_t thread_id;
+    sem_t semaphore;
+} ThreadData;
+
+typedef struct
+{
+    int num_threads;
+    ThreadData *thread_data;
+} ProcessManagerData;
+
+void *encrypt_text(void *data)
+{
+    ThreadData *thread_data = (ThreadData *)data;
+    int pos = 0;
+    for (int i = thread_data->start_index; i < thread_data->end_index; i++)
+    {
+        pos += sprintf(thread_data->encrypted_text + pos, "%d ", (int)thread_data->text[i]);
+    }
+    sem_post(&thread_data->semaphore);
+    return NULL;
+}
+
+void *process_manager(void *data)
+{
+    ProcessManagerData *process_manager_data = (ProcessManagerData *)data;
+    int num_threads = process_manager_data->num_threads;
+    ThreadData *thread_data = process_manager_data->thread_data;
+    char *encrypted_text = (char *)calloc(BUFFER_SIZE * num_threads, sizeof(char));
+
+    int completed_threads = 0;
+
+    while (completed_threads < num_threads)
+    {
+        for (int i = 0; i < num_threads; i++)
+        {
+            int semaphore_value;
+            sem_getvalue(&thread_data[i].semaphore, &semaphore_value);
+            if (semaphore_value == 1)
+            {
+                completed_threads++;
+                printf("Thread %lu has completed\n", (unsigned long)thread_data[i].thread_id);
+                sem_wait(&thread_data[i].semaphore);
+            }
+        }
+        sleep(1);
+    }
+
+    for (int i = 0; i < num_threads; i++)
+    {
+        strcat(encrypted_text, thread_data[i].encrypted_text);
+    }
+
+    printf("Encrypted text: %s\n", encrypted_text);
+
+    // Free the allocated memory
+    free(encrypted_text);
+
+    return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    char *text = "Hello, World!";
+    if (argc > 1)
+    {
+        text = argv[1];
+    }
+
+    printf("Original Text: %s\nStarting...\n", text);
+
+    int segment_size = 4;
+
+    int num_threads = (strlen(text) + segment_size - 1) / segment_size; // Divide by 4 and round up
+
+    pthread_t threads[num_threads];
+    ThreadData thread_data[num_threads];
+    int text_length = strlen(text);
+    int chunk_size = segment_size;
+
+    for (int i = 0; i < num_threads; i++)
+    {
+        int start_index = i * chunk_size;
+        int end_index = (i == num_threads - 1) ? text_length : start_index + chunk_size;
+        thread_data[i].text = text;
+        thread_data[i].start_index = start_index;
+        thread_data[i].end_index = end_index;
+        sem_init(&thread_data[i].semaphore, 0, 0);
+        pthread_create(&threads[i], NULL, encrypt_text, &thread_data[i]);
+        thread_data[i].thread_id = threads[i];
+    }
+
+    sleep(1); // Allow the worker threads to start executing
+
+    pthread_t process_manager_thread;
+    ProcessManagerData process_manager_data = {num_threads, thread_data};
+    pthread_create(&process_manager_thread, NULL, process_manager, &process_manager_data);
+
+    for (int i = 0; i < num_threads; i++)
+    {
+        pthread_join(threads[i], NULL);
+    }
+
+    pthread_join(process_manager_thread, NULL);
+
+    for (int i = 0; i < num_threads; i++)
+    {
+        sem_destroy(&thread_data[i].semaphore);
+    }
+
+    return 0;
+}
